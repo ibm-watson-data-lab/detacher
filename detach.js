4e5b95bb8fc76641d04d2acce6ded5c14e164602
Glynn Bird <glynn.bird@gmail.com>
2017-01-23 12:23:09 +0000

    promises are neater

--- detach.js
+++ detach.js
@@ -1,9 +1,7 @@
 
-var async = require('async');
 var pkgcloud = require('pkgcloud');
 
 /* message should be of the form:
-
 {
  // cloudant credentials 
  "cloudantURL": "https://username:password@hostname.cloudant.com",
@@ -11,18 +9,20 @@ var pkgcloud = require('pkgcloud');
  "id": "mydocumentid",
 
  // object storage credentials
- "auth_url": "https://identity.open.softlayer.com",
- "domainId": "524d4ff43a1b38c6c6718886dfbb279e",
- "domainName": "72244",
- "password": "tfj2ufwbwuGqgw=G",
- "project": "object_storage_123_4567",
- "projectId": "1234567",
- "region": "dallas",
- "role": "admin",
- "userId": "f850a5eb5484ed57b50648693fde445e",
- "username": "admin_325557bde2abb165357707cee6962f6244a062d2"
-}
+ "objectStorageCredentials": {
+  "auth_url": "https://identity.open.softlayer.com",
+  "domainId": "524d4ff43a1b38c6c6718886dfbb279e",
+  "domainName": "72244",
+  "password": "tfj2ufwbwuGqgw=G",
+  "project": "object_storage_123_4567",
+  "projectId": "1234567",
+  "region": "dallas",
+  "role": "admin",
+  "userId": "f850a5eb5484ed57b50648693fde445e",
+  "username": "admin_325557bde2abb165357707cee6962f6244a062d2"
+ }
 
+}
 */
 
 function main(message) {
@@ -39,73 +39,69 @@ function main(message) {
     return { err: errs };
   }
 
-  // setup Cloudant library
-  var cloudant = require('cloudant')(message.cloudantURL);
+  // setup Cloudant library - we need to two Cloudant connections
+  // 1) using the "promises" plugin so we can use the Cloudant library with promises
+  // 2) using hte "default" plugin so we can get a readable stream of the attachment data
+  var cloudant = require('cloudant')({url: message.cloudantURL, plugin: 'promises'});
+  var cloudantStream = require('cloudant')({url: message.cloudantURL});
   var db = cloudant.use(message.dbname);
+  var dbStream = cloudantStream.use(message.dbname);
 
-  // return a Promise
-  return new Promise(function(resolve, reject) {
+  // fetch the JSON document
+  return db.get(message.id).then(function(doc) {
 
-    // fetch the document
-    db.get(message.id, function(err, doc) {
-      if (err) {
-        return reject(err.msg);
-      }
+    // if there are attachments
+    if (typeof doc._attachments === 'object' ) {
+      
+      // our new array of Object Store attachments
+      var attachments = [];
 
-      // if there are attachments
-      if (typeof doc._attachments === 'object' && Object.keys(doc._attachments).length > 0) {
-        
-        var attachments = [];
+      // list of Promises
+      var tasks = [];
 
-        // queue handler
-        var q = async.queue(function(attName, done) {
-          // create a HTTP request to fetch the attachment, but pause it
-          var rs =  db.attachment.get(message.id, attName); 
-          rs.pause();
+      // for each attachment name
+      Object.keys(doc._attachments).forEach(function(attName) {
 
-          // create a HTTP request to fetch the attachment, but pause it
-          var rs =  db.attachment.get(message.id, attName); 
-          rs.pause();
+        // get an HTTP stream of its data and pause it
+        var rs =  dbStream.attachment.get(message.id, attName); 
+        rs.pause();
 
-          // then write this readstream to Object Storage
-          saveFile(message.objectStorageCredentials, rs, 'uploads', doc._id + '-' + attName)
-            .then(function(data) {
-              attachments.push(data);
-              done(null, data)
-            })
-            .catch(function(err) {
-              done(err, null)
-            });
-        }, 3);
+        // save the file to object storage, adding the Promise to the list
+        var p = saveFile(message.objectStorageCredentials, rs, 'uploads', doc._id + '-' + attName).then(function(data) {
+          attachments.push(data);
+        });
+        tasks.push(p);
 
+      });
 
-        q.drain = function() {
-          delete doc._attachments;
-          doc.attachments = attachments;
-          db.insert(doc, function(err, data) {
-            if (err) {
-              return reject(err);
-            }
-            resolve({ doc: doc, data: data });
-          })
-        }
+      // when all the Promises are resolved
+      return Promise.all(tasks).then(function() {
+        // save the document data, removing the Cloudant attachment
+        // but leaving an "attachments" array with references to the 
+        // object storage versions
+        delete doc._attachments;
+        doc.attachments = attachments;
+        return db.insert(doc);
+      }).then(function(data) {
+        return { attachments: attachments, data: data };
+      })
 
-        // add all attachment names to the queue
-        q.push(Object.keys(doc._attachments));
-
-      } else {
-        resolve({msg: 'no attachments found in docId' + message.id});
-      }
-    });
+    } else {
+      return ({msg: 'no attachments found in docId' + message.id});
+    }
 
   });
-
 }
 
 
-
+// save a stream of data to object storage
+// creds - the object storage credentials
+// rs - the ReadStream of data
+// directory - the sub-directory to place the data
+// filename - the filename to save it as
 var saveFile = function(creds, rs, directory, filename) {
 
+  // convert Bluemix Object Storage credentials to pkgcloud format
   var config = {
       provider: 'openstack',
       useServiceCatalog: true,
@@ -118,9 +114,9 @@ var saveFile = function(creds, rs, directory, filename) {
       password: creds.password,
       region: creds.region   
   };
-
   var storageClient = pkgcloud.storage.createClient(config);
 
+  // return a Promise
   return new Promise(function(resolve, reject) {
 
     // authenticate the object storage client library
@@ -132,24 +128,27 @@ var saveFile = function(creds, rs, directory, filename) {
       // create the container
       storageClient.createContainer({name: directory}, function(err, container) {
 
+        // upload the file
         var upload = storageClient.upload({
           container: container.name,
           remote: filename
         });
 
+        // if it fails, reject the Promise
         upload.on('error', function(err) {
           reject(err);
         });
 
+        // if it succeeds, resolve the Promise
         upload.on('success', function(file) {
           resolve(file.toJSON())
         });
 
-        // pipe the readstream to objet storage
+        // pipe the readstream to object storage
         rs.pipe(upload);
         rs.resume();
       });
     });
   });
-}
+};
 
